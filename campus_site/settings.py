5c9f4f3632ac84d5d759455bf77f7b77d64701bb
slimebathtub <slimebathtub@gmail.com>
2025-04-18 07:13:11 +0800

    Add new Django apps: accounts, resources, rooms, and calendar

--- campus_site/settings.py
+++ campus_site/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'core',
+    'resources',
+    'rooms',
+    'calendar',
+    'accounts',
 ]
 
 MIDDLEWARE = [
